647d41ad4883f22d5424c371d9fc1c4fcf25f8cf
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2024-02-11 02:01:59 -0600

    Finished listing assignment in plug in and add files

--- popup.js
+++ popup.js
@@ -1,5 +1,33 @@
 import { getCurrentTab } from "./utils.js";
 
+//function to add all assignments to extension DOM
+const viewAssignments = (assignmentData)=>{
+    const assignemntElements = document.getElementById("assignments");
+    assignemntElements.innerHTML = "";
+    console.log(assignmentData);
+    //if an object exists then add assignments
+    if(assignmentData){
+       Object.keys(assignmentData).forEach(assignmentID=>{
+        const assignment = assignmentData[assignmentID];
+        addNewAssignment(assignemntElements,assignment)
+       })
+    }else{ //if they do not exist then show that there are no assignments to show
+        assignemntElements.innerHTML = '<i class="row">No assignments to show</i>';
+    }
+}
+
+const addNewAssignment = (assignmentElements, assignment) =>{
+    const assignmentTitleElement = document.createElement("div");
+    const newAssignmentElement = document.createElement("div");
+
+    assignmentTitleElement.textContent = assignment.title+"- Due: "+ assignment.dueDate;
+    assignmentTitleElement.className = "assignment-title";
+
+    newAssignmentElement.className = "assignment";
+
+    newAssignmentElement.appendChild(assignmentTitleElement);
+    assignmentElements.appendChild(newAssignmentElement);
+}
 document.addEventListener("DOMContentLoaded", async ()=>{
     const activeTab = await getCurrentTab();
     var pattern = /canvas.*\/courses\/.*\/assignments/;
@@ -13,10 +41,14 @@ document.addEventListener("DOMContentLoaded", async ()=>{
             const courseIDIndex = pathSegments.findIndex(segment => segment === "courses") + 1;
             // Extract the courseId
             const courseId = pathSegments[courseIDIndex];
+            console.log(courseId);
             //get current courseAssignments from chrome storage
-            chrome.storage.sync.get(currentCourse, (data)=>{
-                if(data[currentCourse]){
+            chrome.storage.sync.get(courseId, (data)=>{
+                if(data[courseId]){
+                    //retrieve the assignments for the current course
                     const currentCourseAssignments = data[courseId];
+                    //view assignments in popup
+                    viewAssignments(currentCourseAssignments);
                     
 
                 }
